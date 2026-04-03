e6c8cf328951c9b8287cca91cbaaabb8af7bba97
kilkon <37137153+kilkon@users.noreply.github.com>
2026-04-03 17:07:36 +0900

    Convert app for GitHub Pages and Google Sheets
    # Conflicts:
    #	config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.APP_CONFIG = {
-  sheetsWebhookUrl: "",
+  sheetsWebhookUrl: "https://script.google.com/macros/s/AKfycbxQ-MHe90sDDJaRM4sgwySytDm7VpnPrlMtjjZZ2sikpi-ATMHBCsiAYtOLZT_Lg293/exec",
   qrApiBase: "https://api.qrserver.com/v1/create-qr-code/?size=360x360&format=png&data="
 };
